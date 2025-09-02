21cfb34291c67b725b1e5fa632de274a7fab9886
MargretOluwabunmi <125512083+MargretOluwabunmi@users.noreply.github.com>
2025-09-02 17:13:18 +0100

    first commit

--- app/utils/CoursePlayer.tsx
+++ app/utils/CoursePlayer.tsx
@@ -18,7 +18,7 @@ const CoursePlayer: FC<Props> = ({ videoUrl }) => {
     //     videoId: videoUrl,
     //   })
     axios
-      .post("http://localhost:8000/api/v1/getVdoCipherOTP", {
+      .post("https://ilearnit-server.onrender.com/api/v1/getVdoCipherOTP", {
         videoId: videoUrl,
       })
       .then((res) => {
